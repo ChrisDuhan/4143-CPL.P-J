c07a1d4c5b560622ab39c7d4d64bfe18a17d40e4
Christopher Duhan <ChrisDuhan@users.noreply.github.com>
2017-11-07 22:40:13 -0700

    Update main.java

--- Program 3/main.java
+++ Program 3/main.java
@@ -1,21 +1,45 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 public class main {
-    public main(String[] args) throws IOException
-    {
-	Game myGame = new Game();
-	
-	while(!myGame.checkBoardFull())
-	{
-		myGame.printBoard();
-		while(!myGame.checkWinConditions())
-		{
-			myGame.getPlayedSpot();
-			myGame.play(myGame.rowPlayed, myGame.columnPlayed);
+	public static void main(String[] args) throws IOException {
+
+		boolean keepPlaying = true;
+		Scanner playAgain = new Scanner(System.in);
+		System.out.println("Max Duhan");
+		System.out.println("This program allows two human players to");
+		System.out.println("play a game of Tic-Tac-Toe against each other.");
+		System.out.println("X always goes first and you win by placing");
+		System.out.println("three of your marks in a row, column or diagonal.");
+		System.out.println("To make a move enter the index (0-2) of the row");
+		System.out.println("you want to play in, then the index (0-2) of the");
+		System.out.println("column you want to play in.");
+
+		while (keepPlaying) {
+			Game myGame = new Game();
 			myGame.printBoard();
+			while (!myGame.checkWinConditions() && !myGame.checkBoardFull()) {
+				myGame.getPlayedSpot();
+				myGame.play(myGame.rowPlayed, myGame.columnPlayed);
+				myGame.printBoard();
+			}
+
+			if (myGame.checkWinConditions()) {
+				myGame.nextPlayer();
+				System.out.println(myGame.currentPlayerTurn + " wins!");
+			} else {
+				System.out.println("It's a draw!");
+			}
+			System.out.println("Would you like to play again? (Y/N)");
+			if (playAgain.next().toUpperCase().charAt(0) == 'N') {
+				keepPlaying = false;
+				System.out.println("Bye!");
+				System.exit(0);
+			} else {
+				System.out.println("New game starting!");
+			}
+
 		}
 	}
 
-	
-    }
 }
